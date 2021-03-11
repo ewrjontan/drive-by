0114af1a48c0feeaf950fbfd93d5121d0a3d3330
Jonathan Cardenas <jonscardenas@gmail.com>
2021-03-11 13:28:02 -0600

    finshed index

---
+++ scripts/scripts.js
@@ -0,0 +1,32 @@
+
+
+/*Jquery
+
+$(function(){
+    console.log('Hello world');
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('#navbar-logo').fadeIn();                     
+        }
+    });
+})*/
+
+
+/*-- For Logo Shift upon navbar hitting top of view port --*/
+/*let navbarLogo = document.getElementById("navbarLogo");
+
+let logoShiftOnScroll = function () {
+    let yPos = window.scrollY;
+    console.log(yPos);
+
+    if (yPos >= 48) {
+        navbarLogo.classList.remove("d-none");
+        navbarLogo.classList.add("d-inline");
+    } else {
+        navbarLogo.classList.remove("d-inline");
+        navbarLogo.classList.add("d-none");
+    }
+};
+
+window.addEventListener("scroll", logoShiftOnScroll);*/
